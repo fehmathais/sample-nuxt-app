77df7b89b70c8504cb22ab8b3c6e479127695e70
Felipe Mathais <fmathais@gmail.com>
2019-04-27 21:49:04 -0300

    Added post list and post view.

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,48 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        sample-nuxt-app
-      </h1>
-      <h2 class="subtitle">
-        My supreme Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
+    <table class="table">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Body</th>
+        <td>Actions</td>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="post in posts"
+          >
+        <td>{{post.id}}</td>
+        <td>{{post.title}}</td>
+        <td>{{post.body}}</td>
+        <td>
+          <nuxt-link
+            :to="{name: 'posts-id', params: {id: post.id}}"
+            :key="post.id">
+            View
+          </nuxt-link>
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  import axios from 'axios'
 
-export default {
-  components: {
-    Logo
+  export default {
+    data() {
+      return {
+        posts: []
+      }
+    },
+    mounted() {
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+          this.posts = response.data
+        })
+    }
   }
-}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
---
+++ pages/posts/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <h1>{{post.title}}</h1>
+    <p>{{post.body}}</p>
+  </section>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data() {
+      return {
+        id: this.$route.params.id,
+        post: {}
+      }
+    },
+    mounted() {
+      axios.get(`https://jsonplaceholder.typicode.com/posts/${this.id}`)
+        .then(response => {
+          this.post = response.data
+        })
+    }
+  }
+</script>
